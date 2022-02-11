c1c10547f861c20bd35b07d548935f123a9f21a3
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-02-11 18:55:51 +0700

    v6.4.6-SNAPSHOT | Fix build

--- src/main/java/com/praya/myitems/builder/passive/debuff/DebuffToxic.java
+++ src/main/java/com/praya/myitems/builder/passive/debuff/DebuffToxic.java
@@ -4,36 +4,30 @@
 
 package com.praya.myitems.builder.passive.debuff;
 
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import com.praya.agarthalib.utility.PotionUtil;
-import com.praya.myitems.config.plugin.MainConfig;
-import org.bukkit.entity.Player;
 import api.praya.myitems.builder.passive.PassiveEffectEnum;
 import com.praya.myitems.builder.abs.PassiveEffect;
+import com.praya.myitems.config.plugin.MainConfig;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
 
-public class DebuffToxic extends PassiveEffect
-{
+public class DebuffToxic extends PassiveEffect {
     private static final PassiveEffectEnum debuff;
-    
+
     static {
         debuff = PassiveEffectEnum.TOXIC;
     }
-    
+
     public DebuffToxic() {
         super(DebuffToxic.debuff, 1);
     }
-    
+
     public DebuffToxic(final int grade) {
         super(DebuffToxic.debuff, grade);
     }
-    
+
     @Override
     public final void cast(final Player player) {
         final MainConfig mainConfig = MainConfig.getInstance();
         final PotionEffectType potionType = this.getPotion();
-        final boolean isEnableParticle = mainConfig.isMiscEnableParticlePotion();
-        final PotionEffect potion = PotionUtil.createPotion(potionType, 96000, this.grade, true, isEnableParticle);
-        player.addPotionEffect(potion);
     }
 }
