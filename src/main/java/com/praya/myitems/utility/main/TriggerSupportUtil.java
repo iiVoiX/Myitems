040d5b9b9993f87810d091ec9f676fd4a476378b
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-02-11 17:30:58 +0700

    v6.4.6-SNAPSHOT | Work with AgarthaLib v2.7.3-SNAPSHOT

---
+++ src/main/java/com/praya/myitems/utility/main/TriggerSupportUtil.java
@@ -0,0 +1,25 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package com.praya.myitems.utility.main;
+
+import api.praya.agarthalib.manager.plugin.SupportManagerAPI;
+import org.bukkit.OfflinePlayer;
+import com.praya.agarthalib.utility.PlayerUtil;
+import com.praya.myitems.config.plugin.MainConfig;
+import api.praya.agarthalib.main.AgarthaLibAPI;
+import org.bukkit.entity.Player;
+
+public class TriggerSupportUtil
+{
+    public static final void updateSupport(final Player player) {
+        final AgarthaLibAPI agarthaLibAPI = AgarthaLibAPI.getInstance();
+        final SupportManagerAPI supportManagerAPI = agarthaLibAPI.getPluginManagerAPI().getSupportManager();
+        final MainConfig mainConfig = MainConfig.getInstance();
+        final boolean enableMaxHealth = mainConfig.isStatsEnableMaxHealth();
+        if (enableMaxHealth) {
+            PlayerUtil.setMaxHealth(player);
+        }
+    }
+}
